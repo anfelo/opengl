3c828e09210a1962bafe72f39a5c8b0954867a39
Andres Osorio <af.osorio1341@gmail.com>
2025-02-20 08:04:26 +0100

    render more cubes

--- going-3d/src/main.cpp
+++ going-3d/src/main.cpp
@@ -132,6 +132,19 @@ int main() {
     texture_uniform(&shader_program, "texture1", 0);
     texture_uniform(&shader_program, "texture2", 1);
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f),
+    };
+
     while (!glfwWindowShouldClose(window)) {
         process_input(window);
 
@@ -140,8 +153,8 @@ int main() {
 
         glUniform1f(tex_uniform_id, mixValue);
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+        // glm::mat4 model = glm::mat4(1.0f);
+        // model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
         glm::mat4 view = glm::mat4(1.0f);
         // note that we're translating the scene in the reverse direction of where we want to move
@@ -151,7 +164,6 @@ int main() {
         projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
         // Sets the uniform value
-        glUniformMatrix4fv(model_uniform_loc, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(view_uniform_loc, 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(projection_uniform_loc, 1, GL_FALSE, glm::value_ptr(projection));
 
@@ -161,7 +173,17 @@ int main() {
         shader_program_activate(&shader_program);
         VAO_bind(&VAO1);
 
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        // Draw 10 cubes with different positions and rotations
+        for(unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            glUniformMatrix4fv(model_uniform_loc, 1, GL_FALSE, glm::value_ptr(model));
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
