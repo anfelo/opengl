0f3f336adcc125c4ce017d86f7c3e7809cd74eb1
Andres Osorio <af.osorio1341@gmail.com>
2025-04-12 20:17:46 +0200

    add collision resolution

--- breakout-game/src/game.cpp
+++ breakout-game/src/game.cpp
@@ -98,11 +98,17 @@ void game_process_input(game_t *game, float dt) {
         if (game->keys[GLFW_KEY_A]) {
             if (game->player.position.x >= 0.0f) {
                 game->player.position.x -= velocity;
+                if (game->ball.stuck) {
+                    game->ball.game_object.position.x -= velocity;
+                }
             }
         }
         if (game->keys[GLFW_KEY_D]) {
             if (game->player.position.x <= game->width - game->player.size.x) {
                 game->player.position.x += velocity;
+                if (game->ball.stuck) {
+                    game->ball.game_object.position.x += velocity;
+                }
             }
         }
         if (game->keys[GLFW_KEY_SPACE]) {
@@ -113,6 +119,14 @@ void game_process_input(game_t *game, float dt) {
 
 void game_update(game_t *game, float dt) {
     ball_update(&game->ball, dt, game->width);
+
+    game_do_collisions(game);
+
+    // check loss condition
+    if (game->ball.game_object.position.y >= game->height) {
+        game_reset_level(game);
+        game_reset_player(game);
+    }
 }
 
 void game_render(game_t *game) {
@@ -131,4 +145,188 @@ void game_render(game_t *game) {
         // ball
         game_object_draw(game->renderer, &game->ball.game_object);
     }
+}
+
+void game_reset_level(game_t *game) {
+    if (game->level == 0) {
+        game_level_load(game->resources, &game->levels[0],
+                        "./resources/levels/one.lvl", game->width,
+                        game->height / 2);
+    } else if (game->level == 1) {
+        game_level_load(game->resources, &game->levels[1],
+                        "./resources/levels/two.lvl", game->width,
+                        game->height / 2);
+    } else if (game->level == 2) {
+        game_level_load(game->resources, &game->levels[2],
+                        "./resources/levels/three.lvl", game->width,
+                        game->height / 2);
+    } else if (game->level == 3) {
+        game_level_load(game->resources, &game->levels[3],
+                        "./resources/levels/four.lvl", game->width,
+                        game->height / 2);
+    }
+}
+
+void game_reset_player(game_t *game) {
+    // reset player/ball stats
+    game->player.size = glm::vec2(100.0f, 20.0f);
+    game->player.position =
+        glm::vec2(game->width / 2.0f - game->player.size.x / 2.0f,
+                  game->height - game->player.size.y);
+
+    glm::vec2 ball_pos = game->player.position +
+                         glm::vec2(game->player.size.x / 2.0f - BALL_RADIUS,
+                                   -(BALL_RADIUS * 2.0f));
+    ball_reset(&game->ball, ball_pos, INITIAL_BALL_VELOCITY);
+}
+
+// collision detection
+bool check_collision_AABB_AABB(game_object_t one, game_object_t two);
+collision_t check_collision_AABB_circle(ball_t one, game_object_t two);
+direction_t vector_direction(glm::vec2 closest);
+
+void game_do_collisions(game_t *game) {
+    for (game_object_t &box : game->levels[game->level].bricks) {
+        if (!box.destroyed) {
+            collision_t collision =
+                check_collision_AABB_circle(game->ball, box);
+            if (collision.collided) {
+                // destroy block if not solid
+                if (!box.is_solid) {
+                    box.destroyed = true;
+                }
+                // collision resolution
+                direction_t dir = collision.direction;
+                glm::vec2 diff_vector = collision.diff_vec;
+                // horizontal collision
+                if (dir == LEFT || dir == RIGHT) {
+                    // reverse horizontal velocity
+                    game->ball.game_object.velocity.x =
+                        -game->ball.game_object.velocity.x;
+                    // relocate
+                    float penetration =
+                        game->ball.radius - std::abs(diff_vector.x);
+                    if (dir == LEFT) {
+                        // move ball to right
+                        game->ball.game_object.position.x += penetration;
+                    } else {
+                        // move ball to left;
+                        game->ball.game_object.position.x -= penetration;
+                    }
+                } else {
+                    // vertical collision
+                    // reverse vertical velocity
+                    game->ball.game_object.velocity.y =
+                        -game->ball.game_object.velocity.y;
+                    // relocate
+                    float penetration =
+                        game->ball.radius - std::abs(diff_vector.y);
+                    if (dir == UP) {
+                        // move ball back up
+                        game->ball.game_object.position.y -= penetration;
+                    } else {
+                        // move ball back down
+                        game->ball.game_object.position.y += penetration;
+                    }
+                }
+            }
+        }
+    }
+
+    // check collisions for player pad (unless stuck)
+    collision_t result = check_collision_AABB_circle(game->ball, game->player);
+    if (!game->ball.stuck && result.collided) {
+        // check where it hit the board, and change velocity based on where it
+        // hit the board
+        float centerBoard =
+            game->player.position.x + game->player.size.x / 2.0f;
+        float distance =
+            (game->ball.game_object.position.x + game->ball.radius) -
+            centerBoard;
+        float percentage = distance / (game->player.size.x / 2.0f);
+        // then move accordingly
+        float strength = 2.0f;
+        glm::vec2 old_velocity = game->ball.game_object.velocity;
+        game->ball.game_object.velocity.x =
+            INITIAL_BALL_VELOCITY.x * percentage * strength;
+        // ball->velocity.y = -ball->velocity.y;
+        game->ball.game_object.velocity =
+            // keep speed consistent over both axes
+            // (multiply by length of old velocity, so
+            // total strength is not changed)
+            glm::normalize(game->ball.game_object.velocity) *
+            glm::length(old_velocity);
+        // fix sticky paddle
+        game->ball.game_object.velocity.y =
+            -1.0f * abs(game->ball.game_object.velocity.y);
+    }
+}
+
+// AABB - AABB collision
+bool check_collision_AABB_AABB(game_object_t one, game_object_t two) {
+    // collision x-axis?
+    bool collision_x = one.position.x + one.size.x >= two.position.x &&
+                       two.position.x + two.size.x >= one.position.x;
+    // collision y-axis?
+    bool collision_y = one.position.y + one.size.y >= two.position.y &&
+                       two.position.y + two.size.y >= one.position.y;
+    // collision only if on both axes
+    return collision_x && collision_y;
+}
+
+// AABB - Circle collision
+collision_t check_collision_AABB_circle(ball_t one, game_object_t two) {
+    // get center point circle first
+    glm::vec2 center(one.game_object.position + one.radius);
+    // calculate AABB info (center, half-extents)
+    glm::vec2 aabb_half_extents(two.size.x / 2.0f, two.size.y / 2.0f);
+    glm::vec2 aabb_center(two.position.x + aabb_half_extents.x,
+                          two.position.y + aabb_half_extents.y);
+    // get difference vector between both centers
+    glm::vec2 difference = center - aabb_center;
+    glm::vec2 clamped =
+        glm::clamp(difference, -aabb_half_extents, aabb_half_extents);
+    // add clamped value to AABB_center and we get the value of box closest to
+    // circle
+    glm::vec2 closest = aabb_center + clamped;
+    // retrieve vector between center circle and closest point AABB and check if
+    // length <= radius
+    difference = closest - center;
+
+    // not <= since in that case a collision also occurs when
+    // object one exactly touches object two, which they are at
+    // the end of each collision resolution stage.
+    if (glm::length(difference) < one.radius) {
+        return (collision_t){
+            true,
+            vector_direction(difference),
+            difference,
+        };
+    } else {
+        return (collision_t){
+            false,
+            UP,
+            glm::vec2(0.0f),
+        };
+    }
+}
+
+// calculates which direction a vector is facing (N,E,S or W)
+direction_t vector_direction(glm::vec2 target) {
+    glm::vec2 compass[] = {
+        glm::vec2(0.0f, 1.0f),  // up
+        glm::vec2(1.0f, 0.0f),  // right
+        glm::vec2(0.0f, -1.0f), // down
+        glm::vec2(-1.0f, 0.0f)  // left
+    };
+    float max = 0.0f;
+    unsigned int best_match = -1;
+    for (unsigned int i = 0; i < 4; i++) {
+        float dot_product = glm::dot(glm::normalize(target), compass[i]);
+        if (dot_product > max) {
+            max = dot_product;
+            best_match = i;
+        }
+    }
+    return (direction_t)best_match;
 }
--- breakout-game/src/game.h
+++ breakout-game/src/game.h
@@ -9,6 +9,15 @@
 #include "sprite_renderer.h"
 #include "texture.h"
 
+// Represents the four possible (collision) directions
+typedef enum direction_t { UP, RIGHT, DOWN, LEFT } direction_t;
+// Defines a Collision typedef that represents collision data
+typedef struct collision_t {
+    bool collided;
+    direction_t direction;
+    glm::vec2 diff_vec; // difference vector center - closest point
+} collision_t;
+
 enum game_state_t { GAME_ACTIVE, GAME_MENU, GAME_WIN };
 
 typedef struct game_t {
@@ -36,5 +45,8 @@ void game_init(game_t *game);
 void game_process_input(game_t *game, float dt);
 void game_update(game_t *game, float dt);
 void game_render(game_t *game);
+void game_do_collisions(game_t *game);
+void game_reset_level(game_t *game);
+void game_reset_player(game_t *game);
 
 #endif
